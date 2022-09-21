a1c49bc72c5d134d63214e7e025a25e791084f4b
Faruq51 <113996402+Faruq51@users.noreply.github.com>
2022-09-21 10:51:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ import { watchFile, unwatchFile, readFileSync } from 'fs'
 
 global.owner = 
  [
-  ['+62 858-2234-7348', 'Hyzer'],
-  ['+62 858-2234-7348'],
-  ['+62 858-2234-7348']
+  ['+62 858-0826-4974', 'Hyzer'],
+  ['+62 858-0826-4974'],
+  ['+62 858-0826-4974']
 ]
 
 
 
- // ['6285822347348', '6285822347348', '6285822347348']
+ // ['6285807264974', '6285807264974', '6285807264974']
 
   // [number, dia creator/owner?, dia developer?]
 // Put your number here
@@ -50,14 +50,14 @@ global.APIKeys = { // APIKey Here
 */
 
 // Sticker WM
-global.packname = `\t\t「 ArullOfc 」\n\t\t「 IG: sahrulwara_____ 」\n\t\t\t\t\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`
+global.packname = `\t\t「 gw 」\n\t\t「 IG: sahrulwara_____ 」\n\t\t\t\t\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`
 global.author = ''
 global.thumb = readFileSync('./me.png')
 global.thumb2 = readFileSync('./me2.jpeg')
 global.multiplier = 69 // The higher, The harder levelup
 
 //Text here
-global.me = 'ArullBotz'
+global.me = 'celek'
 global.str = '-------------'
 global.l = '「'
 global.r = '」'
